1546611b9735c2af15f9f0db178530641d306ae3
Ayushman Satpathy <ayushmansatpathy1234@gmail.com>
2025-04-27 16:34:23 -0400

    completed pull

--- backend/main.py
+++ backend/main.py
@@ -46,7 +46,7 @@ class RoomManager:
         usernames = list(self.rooms.get(room_id, {}).keys())
         for ws in self.rooms.get(room_id, {}).values():
             await ws.send_json({"type": "update_players", "players": usernames})
-            
+
     async def start_game(self, room_id: str):
         if room_id not in self.game_states:
             raise HTTPException(status_code=404, detail="Room not found")
@@ -55,32 +55,40 @@ class RoomManager:
         self.game_states[room_id]["pot"] = 0
         self.game_states[room_id]["round_count"] = 1
         await self.broadcast(room_id)
-    
-    async def handle_action(self, room_id: str, username: str, action: str, amount: int = 0):
+
+    async def handle_action(
+        self, room_id: str, username: str, action: str, amount: int = 0
+    ):
         state = self.game_states.get(room_id)
         if not state or not state["game_active"]:
-            raise HTTPException(status_code=400, detail="Game Session Currently Not Active")
-        
+            raise HTTPException(
+                status_code=400, detail="Game Session Currently Not Active"
+            )
+
         if state["players"][state["turn_index"]] != username:
             raise HTTPException(status_code=403, detail="Not yet your turn!")
-        
+
         if action == "bet":
             if state["chip_balances"][username] < amount:
                 raise HTTPException(status_code=400, detail="Insufficient chips")
             state["chip_balances"][username] -= amount
             state["pot"] += amount
-        elif action == "fold": # skipping this player's turn 
-            state["players"].remove(username) # remove from list
+        elif action == "fold":  # skipping this player's turn
+            state["players"].remove(username)  # remove from list
         # moving turn
         state["turn_index"] = (state["turn_index"] + 1) % len(state["players"])
         if len(state["players"]) == 1:  # one player left
-            await self.end_game(room_id, state["players"][0]) # TODO winner caluclation, this is placeholder
+            await self.end_game(
+                room_id, state["players"][0]
+            )  # TODO winner caluclation, this is placeholder
             # reset pot
             state["pot"] = 0
             # if maximum round has been reached
             # hardcoded as a max of 3 rounds
             if state["round_count"] >= 3:
-                await self.end_game(room_id, state["players"][0]) # TODO winner calculation
+                await self.end_game(
+                    room_id, state["players"][0]
+                )  # TODO winner calculation
             else:
                 # changing basic attributes for next round
                 state["round_count"] += 1
@@ -89,13 +97,12 @@ class RoomManager:
                 await self.broadcast(room_id)
         else:
             await self.broadcast(room_id)
-        
-        
+
     async def end_game(self, room_id: str, winner: str):
         state = self.game_states.get(room_id)
         if not state:
             raise HTTPException(status_code=404, detail="Room not found")
-        
+
         state["chip_balances"][winner] += state["pot"]
         # maybe notify all players about who won by sending a json messgage back to frontend?
         state["game_active"] = False
@@ -117,7 +124,7 @@ async def websocket_endpoint(
     await manager.connect(room_id, username, websocket, temp)
     try:
         while True:
-            #await websocket.receive_text()  # we don't use the message, just keep connection open
+            # await websocket.receive_text()  # we don't use the message, just keep connection open
             message = await websocket.receive_json()
             action = message.get("action")
             if action == "start_game":
